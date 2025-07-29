574009b960eed5e4205214f75860f6eb2aac5121
Lian Menzel <lian.menzel@web.de>
2025-07-29 08:54:47 +0200

    fix

--- static/footer.js
+++ static/footer.js
@@ -1,3 +1,5 @@
+const log = console.log;
+
 document.getElementById('backLink').addEventListener('click', function(event) {
     event.preventDefault(); // href erstmal verhindern
 
@@ -7,4 +9,68 @@ document.getElementById('backLink').addEventListener('click', function(event) {
         // Keine History, dann href öffnen
         window.location.href = this.href;
     }
+});
+
+function replace_id_fields_with_proper_fields () {
+    var names = {
+        room_id: {
+            fields: {
+                "building_name": "Gebäudename",
+                "room_name": "Raumname"
+            },
+            label: "Gebäude+Raum",
+            url: "/api/get_room_id?building_name={building_name}&room_name={room_name}"
+        }
+    };
+
+    for (var name of Object.keys(names)) {
+        console.log(name);
+        var element = $('input[name="' + name + '[]"]');
+        var url = names[name].url;
+        console.log(url);
+
+        log(element);
+
+        for (var field in names[name].fields) {
+            var fieldName = names[name].fields[field];
+            var input = $('<input>', {
+                type: 'text',
+                name: field,
+                placeholder: fieldName
+            });
+
+            element.before(input);
+
+            input.on('input', function() {
+                var params = {};
+
+                for (var field of Object.keys(names[name].fields)) {
+                    var value = $(this).closest('form').find('input[name="' + field + '"]').val();
+                    params[field] = value;
+                }
+                var queryString = $.param(params);
+                var newUrl = url.replace(/\{(\w+)\}/g, function(match, p1) {
+                    return params[p1] || '';
+                });
+                log(newUrl);
+
+                $.get(newUrl, function(data) {
+                    log(data);
+                    if (data && data.room_id) {
+                        element.val(data.room_id);
+                    }
+                }).fail(function() {
+                    log("Fehler beim Abrufen der Raum-ID");
+                    element.val('');
+                }
+                );
+            });
+
+            $(element).hide();
+        }
+    }
+}
+
+$( document ).ready(function() {
+    replace_id_fields_with_proper_fields();
 });
