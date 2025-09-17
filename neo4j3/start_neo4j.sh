25001b239360f036d7483e57048aef87e3ba0ae8
Norman Koch <norman.koch@tu-dresden.de>
2025-09-17 09:18:06 +0200

    persistent storage

--- neo4j3/start_neo4j.sh
+++ neo4j3/start_neo4j.sh
@@ -1,1 +1,3 @@
-docker run --rm -p7474:7474 -p7687:7687 -e NEO4J_AUTH=neo4j/test1234 neo4j:5.12
+#!/bin/bash
+
+docker run --name neo4j-db -p7474:7474 -p7687:7687 -e NEO4J_AUTH=neo4j/test1234 -v neo4j_data:/data neo4j:5.12
