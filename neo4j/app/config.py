efbf61c28b8ecf8cd122a82395186c498d1bd74d
n <norman.koch@tu-dresden.de>
2025-09-18 18:41:01 +0200

    removed older code

--- neo4j/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-    NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
-    NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
-    NEO4J_PASS = os.getenv("NEO4J_PASS", "test1234")
-
--- neo4j3/start_neo4j.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --name neo4j-db -p7474:7474 -p7687:7687 -e NEO4J_AUTH=neo4j/test1234 -v neo4j_data:/data neo4j:5.12
