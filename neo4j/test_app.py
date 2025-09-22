a621979b6d21308726d24dbe68fcdcb65603422a
Norman Koch <norman.koch@tu-dresden.de>
2025-09-22 14:24:44 +0200

    new test

--- neo4j/test_app.py
+++ neo4j/test_app.py
@@ -2559,5 +2559,25 @@ class TestNeo4jApp(unittest.TestCase):
             resp = client.post('/save_mapping', json=mapping)
             self.assertEqual(resp.status_code, 200)
 
+    def test_save_mapping_complex_missing_some_fields(self):
+        """Manche Zeilen haben fehlende Properties, trotzdem Merge möglich."""
+        csv_data = "person,city,country\nAlice,Berlin,Deutschland\nBob,,Deutschland"
+        mapping = {
+            "nodes": {
+                "Person": [{"original": "person", "renamed": "name"}],
+                "Ort": [{"original": "city", "renamed": "stadt"}],
+                "Land": [{"original": "country", "renamed": "name"}]
+            },
+            "relationships": [
+                {"from": "Person", "to": "Ort", "type": "WOHNT_IN"},
+                {"from": "Ort", "to": "Land", "type": "LIEGT_IN"}
+            ]
+        }
+        with self.app as client:
+            with client.session_transaction() as sess:
+                sess['raw_data'] = csv_data
+            resp = client.post('/save_mapping', json=mapping)
+            self.assertEqual(resp.status_code, 200)
+
 if __name__ == '__main__':
     unittest.main()
