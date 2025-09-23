dd5711035e7ea64beb4ffcab66fc801f6cf88773
Norman Koch <norman.koch@tu-dresden.de>
2025-09-23 11:24:59 +0200

    outsourced reset_and_load_data

--- neo4j/app.py
+++ neo4j/app.py
@@ -978,26 +978,6 @@ def add_property_to_nodes():
 def fn_debug(label, data):
     print(f"DEBUG [reset_load]: {label}: {data}")
 
-def fn_clear_database():
-    query = """
-        MATCH (n)
-        DETACH DELETE n
-    """
-    fn_debug("Clearing database", "Deleting all nodes and relationships")
-    graph.run(query)
-    fn_debug("Database cleared", "All nodes and relationships removed")
-
-def fn_create_person(vorname, nachname):
-    query = """
-        CREATE (p:Person {vorname:$vorname, nachname:$nachname})
-        RETURN ID(p) AS id
-    """
-    fn_debug("Creating person", {"vorname": vorname, "nachname": nachname})
-    result = graph.run(query, vorname=vorname, nachname=nachname).data()
-    person_id = result[0]["id"]
-    fn_debug("Created person ID", person_id)
-    return person_id
-
 def fn_create_city(stadt):
     query = """
         MERGE (s:Stadt {name:$stadt})
@@ -1008,56 +988,6 @@ def fn_create_city(stadt):
     city_id = result[0]["id"]
     fn_debug("City ID", city_id)
     return city_id
-
-def fn_create_address(street, plz):
-    query = """
-        CREATE (o:Ort {straße:$street, plz:$plz})
-        RETURN ID(o) AS id
-    """
-    fn_debug("Creating address", {"straße": street, "plz": plz})
-    result = graph.run(query, street=street, plz=plz).data()
-    addr_id = result[0]["id"]
-    fn_debug("Address ID", addr_id)
-    return addr_id
-
-def fn_create_book(title, year):
-    query = """
-        CREATE (b:Buch {titel:$title, erscheinungsjahr:$year})
-        RETURN ID(b) AS id
-    """
-    fn_debug("Creating book", {"titel": title, "erscheinungsjahr": year})
-    result = graph.run(query, title=title, year=year).data()
-    book_id = result[0]["id"]
-    fn_debug("Book ID", book_id)
-    return book_id
-
-def fn_create_relation_has_written(person_vorname, person_nachname, book_title):
-    query = """
-        MATCH (p:Person {vorname:$vorname, nachname:$nachname})
-        MATCH (b:Buch {titel:$title})
-        MERGE (p)-[:HAT_GESCHRIEBEN]->(b)
-    """
-    fn_debug("Creating HAT_GESCHRIEBEN relation", {"person": f"{person_vorname} {person_nachname}", "buch": book_title})
-    graph.run(query, vorname=person_vorname, nachname=person_nachname, title=book_title)
-
-def fn_create_relation_lives_in(person_vorname, person_nachname, ort_id):
-    query = """
-        MATCH (p:Person {vorname:$vorname, nachname:$nachname})
-        MATCH (o:Ort) WHERE ID(o)=$ort_id
-        MERGE (p)-[:WOHNT_IN]->(o)
-    """
-    fn_debug("Creating WOHNT_IN relation", {"person": f"{person_vorname} {person_nachname}", "ort_id": ort_id})
-    graph.run(query, vorname=person_vorname, nachname=person_nachname, ort_id=ort_id)
-
-def fn_create_relation_located_in(ort_id, stadt_name):
-    query = """
-        MATCH (o:Ort) WHERE ID(o)=$ort_id
-        MATCH (s:Stadt {name:$stadt})
-        MERGE (o)-[:LIEGT_IN]->(s)
-    """
-    fn_debug("Creating LIEGT_IN relation", {"ort_id": ort_id, "stadt": stadt_name})
-    graph.run(query, ort_id=ort_id, stadt=stadt_name)
-
 
 if __name__ == '__main__':
     try:
