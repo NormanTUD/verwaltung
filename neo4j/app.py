7bbf4659694abc9f4145f743f37afd134f547ab0
Norman Koch <norman.koch@tu-dresden.de>
2025-09-23 14:05:43 +0200

    added update node api

--- neo4j/app.py
+++ neo4j/app.py
@@ -17,6 +17,7 @@ from api.delete_nodes import create_delete_nodes_bp
 from api.create_node import create_create_node_bp
 from api.add_property_to_nodes import create_add_property_to_nodes_bp
 from api.delete_all import create_delete_all_bp
+from api.update_node import create_update_node_bp
 
 from rich.console import Console
 
@@ -66,6 +67,7 @@ app.register_blueprint(create_add_property_to_nodes_bp(graph), url_prefix='/api'
 app.register_blueprint(create_delete_nodes_bp(graph), url_prefix='/api')
 app.register_blueprint(create_create_node_bp(graph), url_prefix='/api')
 app.register_blueprint(create_delete_all_bp(graph), url_prefix='/api')
+app.register_blueprint(create_update_node_bp(graph), url_prefix='/api')
 
 # Definiere den Dateipfad
 SAVED_QUERIES_FILE = 'saved_queries.json'
@@ -532,26 +534,7 @@ def run_query(graph, query, labels, limit):
     print(f"✅ Abfrage erfolgreich, {len(results)} Ergebnisse erhalten")
     return results
 
-@app.route('/api/update_node/<int:node_id>', methods=['PUT'])
-def update_node(node_id):
-    data = request.get_json()
-    property_name = data.get('property')
-    new_value = data.get('value')
 
-    if not graph:
-        return jsonify({"status": "error", "message": "Datenbank nicht verbunden."}), 500
-
-    # Abfrage, um den Node zu finden und das Property zu aktualisieren
-    query = f"""
-        MATCH (n) WHERE ID(n) = {node_id}
-        SET n.{property_name} = $new_value
-        RETURN n
-    """
-    try:
-        graph.run(query, new_value=new_value)
-        return jsonify({"status": "success", "message": f"Node {node_id} wurde aktualisiert."})
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
 
 # ------------------------
 # Hilfsfunktionen
