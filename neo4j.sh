be89df870e612e356725e8d476ed12867ae76766
Lian Menzel <lian.menzel@web.de>
2025-10-13 08:32:13 +0200

    fix

--- neo4j.sh
+++ neo4j.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
 
-docker start neo4j-db 2>/dev/null || \
-docker run --name neo4j-db \
-  -p7474:7474 -p7687:7687 \
-  -e NEO4J_AUTH=neo4j/testTEST12345678 \
-  -v neo4j_data:/data \
-  neo4j:5.14
+docker start neo4j-db 2>/dev/null
+docker run --name neo4j-db -p7474:7474 -p7687:7687 -e NEO4J_AUTH=neo4j/testTEST12345678 -v neo4j_data:/data neo4j:5.14
