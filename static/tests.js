009894055c177daddf0b327d8031e83b3f0ed672
Lian Menzel <lian.menzel@web.de>
2025-10-13 15:09:20 +0200

    new tests

--- static/tests.js
+++ static/tests.js
@@ -1,3 +1,18 @@
+function import_person () {
+    $(".preset-buttons").find("button").first().click()
+    $("form").find("button").last().click()
+}
+
+function assign_person_to_nodes() {
+    $(".from-node-select").first().val("Person")
+    $(".rel-type-input").val("dasisteintest")
+}
+
+function overview () {
+    // Set checkbox
+    $("#querySelection").find("input").first().prop("checked", true)
+}
+
 function run_tests() {
     console.log("Running tests...");
     // Add your test cases here
