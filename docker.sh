94dfd9fc0d4ab0d33a70e9133f1a2df12744a21e
Norman Koch <norman.koch@tu-dresden.de>
2025-10-15 07:16:44 +0200

    removed php stuff

--- docker.sh
+++ docker.sh
@@ -2,14 +2,11 @@
 
 # Default values
 run_tests=0
-LOCAL_PORT=""
 
 # Help message
 help_message() {
 	echo "Usage: docker.sh [OPTIONS]"
 	echo "Options:"
-	echo "  --local-port       Local port to bind for the GUI"
-	echo "  --run_tests        Run tests before starting"
 	echo "  --help             Show this help message"
 }
 
@@ -24,14 +21,6 @@ _sudo() {
 # Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
 	case $1 in
-		--run_tests)
-			run_tests=1
-			shift
-			;;
-		--local-port)
-			LOCAL_PORT="$2"
-			shift
-			;;
 		--help)
 			help_message
 			exit 0
@@ -43,13 +32,6 @@ while [[ "$#" -gt 0 ]]; do
 	esac
 	shift
 done
-
-# Check for required parameters
-if [[ -z $LOCAL_PORT ]]; then
-	echo "Error: Missing required parameter --local-port. Use --help for usage."
-	exit 1
-fi
-
 
 is_package_installed() {
 	dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -c "ok installed"
@@ -111,42 +93,6 @@ if ! command -v git &>/dev/null; then
 	}
 fi
 
-git rev-parse HEAD > git_hash
-
-export LOCAL_PORT
-
-# Write environment variables to .env file
-echo "#!/bin/bash" > .env
-echo "LOCAL_PORT=$LOCAL_PORT" >> .env
-
-echo "=== Current git hash before auto-pulling ==="
-git rev-parse HEAD
-echo "=== Current git hash before auto-pulling ==="
-
-git pull
-
-function die {
-	echo $1
-	exit 1
-}
-
-SYNTAX_ERRORS=0
-if command -v php 2>/dev/null >/dev/null; then
-	{ for i in $(ls *.php); do if ! php -l $i 2>&1; then SYNTAX_ERRORS=1; fi ; done } | 2>&1 grep -v mongodb
-else
-	echo "php not installed. Not running php -l tests"
-fi
-
-if [[ "$SYNTAX_ERRORS" -ne "0" ]]; then
-	echo "Tests failed";
-	exit 1
-fi
-
-
-if [[ "$run_tests" -eq "1" ]]; then
-	php testing.php && echo "Syntax checks for PHP Ok" || die "Syntax Checks for PHP failed"
-fi
-
 function docker_compose {
     # check if user is in docker group
     if [[ -n $USER ]]; then
@@ -171,15 +117,11 @@ function docker_compose {
 }
 
 docker_compose build || {
-	rm git_hash
 	echo "Failed to build container"
 	exit 254
 }
 
 docker_compose up -d || {
-	rm git_hash
 	echo "Failed to build container"
 	exit 255
 }
-
-rm git_hash
