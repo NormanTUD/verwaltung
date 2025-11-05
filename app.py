9800830f82f716575cbe9a65a191775f86c72a89
Your Name <you@example.com>
2025-11-05 09:46:45 +0100

    default

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ auto_is_authenticated = False
 
 parser = argparse.ArgumentParser(description="Starte die Flask-App mit konfigurierbaren Optionen.")
 parser.add_argument('--debug', action='store_true', help='Aktiviere den Debug-Modus')
-parser.add_argument('--disable_login', action='store_true', help='Deaktivier den Login')
+parser.add_argument('--disable_login', action='store_true', help='Deaktivier den Login', default=False)
 parser.add_argument('--port', type=int, default=5000, help='Port für die App (Standard: 5000)')
 parser.add_argument('--engine-db', type=str, default='sqlite:///instance/database.db', help='URI für create_engine()')
 args = parser.parse_args()
