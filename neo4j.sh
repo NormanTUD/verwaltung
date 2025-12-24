1935d08db6817601e9649072a7ea33f76e73a3cb
n <norman.koch@tu-dresden.de>
2025-12-24 11:25:37 +0100

    fix

--- neo4j.sh
+++ neo4j.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "Starting neo4j"
-docker start neo4j-db
-echo "Starting neo4j: $?"
+#echo "Starting neo4j"
+#docker start neo4j-db
+#echo "Starting neo4j: $?"
 
 echo "Running neo4j"
 docker run --name neo4j-db -p7474:7474 -p7687:7687 -e NEO4J_AUTH=neo4j/testTEST12345678 -v neo4j_data:/data neo4j:5.14
